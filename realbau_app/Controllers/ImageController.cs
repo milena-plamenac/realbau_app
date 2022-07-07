4a9501e40f9e2c430e101e92eb3b0b93e4c5ce68
Milena Plamenac <pmilena@ucg.ac.me>
2022-07-07 15:13:13 +0200

    images

---
+++ realbau_app/Controllers/ImageController.cs
@@ -0,0 +1,71 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace realbau_app.Controllers
+{
+    public class ImageController : Controller
+    {
+        public IActionResult Index()
+        {
+            string filePath = "C:\\Users\\lfili\\Downloads\\people-office.jpg";
+            try
+            {
+                /* Load pre-authorized user credentials from the environment.
+                 TODO(developer) - See https://developers.google.com/identity for
+                 guides on implementing OAuth2 for your application. */
+                GoogleCredential credential = GoogleCredential.GetApplicationDefault()
+                    .CreateScoped(DriveService.Scope.Drive);
+
+                // Create Drive API service.
+                var service = new DriveService(new BaseClientService.Initializer
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = "Drive API Snippets"
+                });
+
+                // Upload file photo.jpg on drive.
+                var fileMetadata = new Google.Apis.Drive.v3.Data.File()
+                {
+                    Name = "photo.jpg"
+                };
+                FilesResource.CreateMediaUpload request;
+                // Create a new file on drive.
+                using (var stream = new FileStream(filePath,
+                           FileMode.Open))
+                {
+                    // Create a new file, with metadata and stream.
+                    request = service.Files.Create(
+                        fileMetadata, stream, "image/jpeg");
+                    request.Fields = "id";
+                    request.Upload();
+                }
+
+                var file = request.ResponseBody;
+                // Prints the uploaded file id.
+                Console.WriteLine("File ID: " + file.Id);
+                return View("File found! Id: " + file.Id);
+            }
+            catch (Exception e)
+            {
+                // TODO(developer) - handle error appropriately
+                if (e is AggregateException)
+                {
+                    Console.WriteLine("Credential Not found");
+                }
+                else if (e is FileNotFoundException)
+                {
+                    Console.WriteLine("File not found");
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return View("File not found");
+
+
+        }
+    }
+}
